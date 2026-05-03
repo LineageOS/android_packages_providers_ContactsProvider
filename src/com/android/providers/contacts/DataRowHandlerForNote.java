48149493947b89699c4e158a22739b5182136b57
Duy Truong <duytruong@google.com>
2026-05-02 22:24:01 -0700

    Restrict max size for Note.NOTE field
    DataRowHandlerForNote allowed unbounded data injection into the note field, which could be exploited by a malicious app with WRITE_CONTACTS permission to exhaust device storage. This change implements size validation by truncating the note field to 10 KB, consistent with other contact fields.
    
    Note: This was autogenerated by MendIt (go/androidmendit)
    
    Bug: 486763102
    Test: atest ContactsProviderTests
    Flag: EXEMPT BUGFIX
    (cherry picked from commit 9da7ba23aa9526cee0fc6a8e29bc34771698f352)
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:c9a98b6082e3bd9e9a52ff71e5f635114b69f4c5
    Merged-In: I6674fb68af01fa57356595d7a928ee554b5b4dcb
    Change-Id: I6674fb68af01fa57356595d7a928ee554b5b4dcb

--- src/com/android/providers/contacts/DataRowHandlerForNote.java
+++ src/com/android/providers/contacts/DataRowHandlerForNote.java
@@ -17,6 +17,8 @@ package com.android.providers.contacts;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.provider.ContactsContract.CommonDataKinds.Note;
 import com.android.providers.contacts.SearchIndexManager.IndexBuilder;
 import com.android.providers.contacts.aggregation.AbstractContactAggregator;
@@ -29,6 +31,20 @@ public class DataRowHandlerForNote extends DataRowHandler {
     public DataRowHandlerForNote(Context context, ContactsDatabaseHelper dbHelper,
             AbstractContactAggregator aggregator) {
         super(context, dbHelper, aggregator, Note.CONTENT_ITEM_TYPE);
+    }
+
+    @Override
+    public long insert(SQLiteDatabase db, TransactionContext txContext, long rawContactId,
+            ContentValues values) {
+        applySimpleFieldMaxSize(values, Note.NOTE);
+        return super.insert(db, txContext, rawContactId, values);
+    }
+
+    @Override
+    public boolean update(SQLiteDatabase db, TransactionContext txContext, ContentValues values,
+            Cursor c, boolean callerIsSyncAdapter) {
+        applySimpleFieldMaxSize(values, Note.NOTE);
+        return super.update(db, txContext, values, c, callerIsSyncAdapter);
     }
 
     @Override
